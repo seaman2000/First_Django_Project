1d13059cb5e6e28383d40a651783b824fd20982c
Georgi Angelov <gtangelov76@gmail.com>
2026-03-31 19:29:58 +0300

    add

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Match(models.Model):
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    odds = models.DecimalField(max_digits=5, decimal_places=2)
+    match_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.home_team} vs {self.away_team}"
--- web/views.py
+++ web/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Match
 
 def home(request):
-    context = {
-        'name': 'Георги',
-        'message': 'Това е динамично съдържание 😎'
-    }
-    return render(request, 'home.html', context)
+    if request.method == 'POST':
+        home_team = request.POST.get('home_team')
+        away_team = request.POST.get('away_team')
+        odds = request.POST.get('odds')
+
+        Match.objects.create(
+            home_team=home_team,
+            away_team=away_team,
+            odds=odds,
+        )
+
+        return redirect('home')
+
+    matches = Match.objects.all()
+
+    return render(request, 'home.html', {'matches': matches})
