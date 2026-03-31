62d2a5169545b33dbdf22088e7616b16af22b236
Georgi Angelov <gtangelov76@gmail.com>
2026-03-31 19:39:56 +0300

    added delete button

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),
+    path('', views.home, name='home'),
+    path('delete/<int:id>/', views.delete_match, name='delete_match'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Match
 
+
 def home(request):
     if request.method == 'POST':
         home_team = request.POST.get('home_team')
         away_team = request.POST.get('away_team')
         odds = request.POST.get('odds')
+        match_date = request.POST.get('match_date')
 
         Match.objects.create(
             home_team=home_team,
             away_team=away_team,
             odds=odds,
+            match_date=match_date,
         )
 
         return redirect('home')
 
     matches = Match.objects.all()
-
     return render(request, 'home.html', {'matches': matches})
+
+
+def delete_match(request, id):
+    match = Match.objects.get(id=id)
+    match.delete()
+    return redirect('home')
